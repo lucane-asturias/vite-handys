78fc3bfc00bb6788bb3c7dbf2e535fda8af57ce3
Lucane <lucassanntos01@gmail.com>
2023-08-25 22:30:06 -0300

    unit: ProductDetail.vue tested

--- tests/unit/views/product-detail.spec.js
+++ tests/unit/views/product-detail.spec.js
@@ -1,23 +1,48 @@
 import { shallowMount, RouterLinkStub } from '@vue/test-utils'
 import { createPinia, setActivePinia } from 'pinia'
+import { createTestingPinia } from '@pinia/testing'
 import ProductDetail from '@/views/ProductDetail.vue'
+
+import { productsMockData } from '../__mocks__/mock-data/products-data'
+import { useProductStore } from '@/store/productStore'
 
 describe('ProductDetail.vue component', () => {
   var formatPrice = vi.fn(num => num)
+  var mockRouter = { push: vi.fn() }
 
   let wrapper
+  let productStore
   // SETUP - run prior to each unit test
   beforeEach(() => {
     setActivePinia(createPinia())
 
+    productStore = useProductStore()
+
     wrapper = shallowMount(ProductDetail, {
       props: { id: '101' },
-      global: { components: { 'router-link': RouterLinkStub }}
+      global: { 
+        components: { 'router-link': RouterLinkStub },
+        plugins: [createTestingPinia({
+          initialState: {
+            productStore: { products: productsMockData }
+          }
+        })]
+      },
     })
   })
 
-  test('should match snapshot', () => {
-    console.log(wrapper.html())
+  afterEach(() => wrapper.unmount())
+
+  test('should match snapshot', () => expect(wrapper.html()).toMatchSnapshot())
+
+  test('should render product info correctly', () => {
+    expect(wrapper.find('p').text()).toBe('Samsung AAZ8')
+    expect(wrapper.find('.alert').text()).toBe('Price: $22,000.00')
+  })
+
+  test('to prop should push to catalog page whenever router-link is clicked', () => {
+    const expectedObj = { name: 'catalog' }
+    expect(wrapper.findComponent(RouterLinkStub).props().to).toMatchObject(expectedObj)
   })
 
 })
